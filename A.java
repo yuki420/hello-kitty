0715c30344bafc4818787d24381a05c39dbec6ce
Yuki420 <yuqing420@163.com>
2012-06-27 20:05:21 +0800

    add A.java

---
+++ A.java
@@ -0,0 +1,19 @@
+package honey;
+
+public class A {
+	
+	public static void main(String[] args) {
+		//This is a sample 
+		System.out.print("Hello World!"); //This is a comment.
+		System.out.println("How are you?");
+		String bob;
+		int hour, minute;
+		bob = "Hello";
+		hour = 11;
+		minute = 59;
+		System.out.println(bob);
+		System.out.println("The current time is hour:"+hour+" minute:"+ minute);
+		
+	}
+}
+
